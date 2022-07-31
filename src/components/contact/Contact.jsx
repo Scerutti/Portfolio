4868d30c87e552134fa85aa488399e998de353cd
Scerutti <softsys95@gmail.com>
2022-07-31 13:19:23 -0300

    Finishing the mail form api

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,10 +11,10 @@ const Contact = () => {
     setMessage(true);
     emailjs
       .sendForm(
-        'service_k2qawqh',
-        'template_c6rkpn6',
+        'service_yylr2rn',
+        'template_th8zk3c',
         formRef.current,
-        'X7K7ebhIeOy3YwHki'
+        'tE17ckht2NDLED0-H'
       )
       .then(
         (result) => {
