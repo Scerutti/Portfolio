4afa282b56de4716f351c203eb4df5c813a02c0e
scerutti <scerutti@proguidemc.com>
2023-03-04 17:33:34 -0300

    ✓ Se resuelve inconveniente al scrollear ✓ Tambien se agrega al navbar el icono y redireccion a "Testimonios"

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,7 +8,7 @@ const HeaderSocials = () => {
   const lenguage = useSelector(state => state.lenguage);
 
   const handleLanguageChange = (e) => {
-    if (e === 'ENG') {
+    if (e === 'ESP') {
       dispatch(changeLenguage(true))
     } else {
       dispatch(changeLenguage(false))
@@ -17,7 +17,7 @@ const HeaderSocials = () => {
 
   return (
     <div className="header__socials">
-      <a href='#' onClick={(e) => handleLanguageChange(e.target.outerText)} title={!lenguage ? "Change Lenguage" : "Cambiar idioma"}>{!lenguage ? 'ENG' : 'ESP'}</a>
+      <a href='#' onClick={(e) => handleLanguageChange(e.target.outerText)} title={!lenguage ? "Change Lenguage" : "Cambiar idioma"}>{!lenguage ? 'ESP' : 'ENG'}</a>
       {
         SocialsLinks.map((social, index) => (
           <a href={social.link} target="_blank" rel='noreferrer' key={index + 123} title={social.title}>{social.icon}</a>
