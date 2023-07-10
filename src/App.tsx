17bcd3db19030009fcb423c13dabba4a10e84838
Sebastián Cerutti <softsys95@gmail.com>
2023-07-09 22:57:15 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CompletePage from './pages/complete-page/CompletePage';
 import Error from './pages/error/Error';
 import ErrorPage from './pages/error/ErrorPage';
-import CardContainer from './pages/card-container/CardContainer';
+// import CardContainer from './pages/card-container/CardContainer';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CompletePage />} />
-        <Route path="/portfolio/:id" element={<CardContainer />} />
+        {/* <Route path="/portfolio/:id" element={<CardContainer />} /> */}
         <Route path="/error" element={<Error />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
