1d9b4f5327b2ec05cd7e52417617bb6d027ddbbf
Sebastián Cerutti <softsys95@gmail.com>
2023-07-16 18:05:39 -0300

    se agrego un test mas

--- src/__test__/pages/Testimonials.test.tsx
+++ src/__test__/pages/Testimonials.test.tsx
@@ -6,4 +6,12 @@ describe("RecomendationCarrousel", () => {
     it("Deberia renderizar el carrousel", () => {
         render(<RecomendationCarrousel />, { wrapper: MemoryReduxProvider })
     })
+    it('displays the title and subtitle correctly', () => {
+        const { getByText } = render(<RecomendationCarrousel />, { wrapper: MemoryReduxProvider });
+        const title = getByText('Feedback from my peers'); // Reemplaza 'Your Title Here' con el título real que esperas
+        const subtitle = getByText('Testimonials'); // Reemplaza 'Your Subtitle Here' con el subtítulo real que esperas
+
+        expect(title).toBeInTheDocument();
+        expect(subtitle).toBeInTheDocument();
+    });
 })
